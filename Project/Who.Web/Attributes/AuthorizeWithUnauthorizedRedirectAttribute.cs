2c20eafda6f84708930ce227ff957efc83f5bf55
thomas.lefeverteughels <thomas.lefever-teughels@axxes.com>
2018-09-08 14:16:47 +0200

    AuthorizeAttributeUnauthorizedRedirect renamed to AuthorizeWithUnauthorizedRedirectAttribute

--- Project/Who.Web/Attributes/AuthorizeWithUnauthorizedRedirectAttribute.cs
+++ Project/Who.Web/Attributes/AuthorizeWithUnauthorizedRedirectAttribute.cs
@@ -3,7 +3,7 @@ using System.Web.Routing;
 
 namespace Who.Web
 {
-    public class AuthorizeAttributeUnauthorizedRedirect : AuthorizeAttribute
+    public class AuthorizeWithUnauthorizedRedirectAttribute : AuthorizeAttribute
     {
         protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
         {
